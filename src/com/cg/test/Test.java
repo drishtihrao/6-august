ffb03b7570a857c5eea791d8e8d32c34018ca0f3
drishtihrao <drishtihrao@gmail.com>
2018-08-06 08:13:50 -0400

    Add files via upload

--- src/com/cg/test/Test.java
+++ src/com/cg/test/Test.java
@@ -1,15 +1,16 @@
 package com.cg.test;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import com.cg.factory.MessageFactory;
 import com.cg.renderer.MessageRenderer;
 
 public class Test {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		MessageRenderer renderer = (MessageRenderer) context.getBean("renderer");
-		renderer.render();
+		try {
+			MessageRenderer renderer = MessageFactory.getMessageRenderer();
+			renderer.render();
+		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
+			e.printStackTrace();
+		} 
 	}
 }
